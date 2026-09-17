d9982b1dd5933bfe1897e1dd34a3a1e40c5552fd
emiraydgn <emiraydogan03@gmail.com>
2026-09-18 00:59:51 +0300

    [TASK-1] Add input handling

--- emir.aydogan/task-1/cmd/service/main.go
+++ emir.aydogan/task-1/cmd/service/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var first, second int
+	var operator string
+
+	_, err := fmt.Scan(&first)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err = fmt.Scan(&second)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	_, err = fmt.Scan(&operator)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
 }
